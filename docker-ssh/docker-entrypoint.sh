b3dcd1c1e8702896d7e00ee45c0958ecb4e7e84a
akh <git@xar.io>
2017-06-07 20:22:53 +0200

    [docker-ssh] Initial commit

--- docker-ssh/docker-entrypoint.sh
+++ docker-ssh/docker-entrypoint.sh
@@ -14,7 +14,7 @@ fi
 
 # if we have "--link some-docker:docker" and not DOCKER_HOST, let's set DOCKER_HOST automatically
 if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
-	export DOCKER_HOST='tcp://docker:2375'
+	export DOCKER_HOST='tcp://freenas:2375'
 fi
 
 echo "export DOCKER_HOST='${DOCKER_HOST}'" >> /etc/profile
